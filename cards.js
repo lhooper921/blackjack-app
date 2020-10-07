75c280bddc1a455a958760495dbcbab95458cc45
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-10-06 19:59:50 -0700

    created shuffle and deal

--- cards.js
+++ cards.js
@@ -23,26 +23,29 @@ class Deck {
         }
         return this.deck.length;
     }
+    shuffle() {
+        let counter = this.deck.length, temp, i;
+        while(counter) {
+            i = Math.floor(Math.random() * counter--);
+            temp = this.deck[counter];
+            this.deck[counter] = this.deck[i];
+            this.deck[i] = temp;
+        }
+        return this.deck;
+    }
+    
+    deal() {
+        let hand = [];
+        while(hand.length < 2) {
+            hand.push(this.deck.pop());
+        }
+        return hand;
+    }
 }
 
-shuffle() {
-    let counter = this.deck.length, temp, i;
-    while(counter) {
-        i = Math.floor(Math.random() * counter--);
-        temp = this.deck[counter];
-        this.deck[counter] = this.deck[i];
-        this.deck[i] = temp;
-    }
-    return this.deck;
-}
 
-deal() {
-    let hand = [];
-    while(hand.length < 2) {
-        hand.push(this.deck.pop());
-    }
-    return hand;
-}
+
+
 
 let deck = new Deck();
 // deck.createDeck(suits, values);
