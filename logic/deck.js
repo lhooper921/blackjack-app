47300172cf89ab2647ca05e88a2473495c4ebb8b
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-10-10 12:17:35 -0700

    created functions for deal and player numbers

--- logic/deck.js
+++ logic/deck.js
@@ -1,11 +1,12 @@
-var suits = ["spades", "diamonds", "clubs", "hearts"];
+var suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let hand = [];
 
 class Card {
-    constructor(suit, value) {
+    constructor(suit, value, weight) {
         this.suit = suit;
         this.value = value;
+        this.weight = weight;
     }
 }
 
@@ -19,7 +20,12 @@ class Deck {
     createDeck() {
         for (let suit of suits) {
             for (let value of values) {
-                this.deck.push(new Card(suit, value));
+                var weight = parseInt(value);
+                if (value == "J" || value == "Q" || value == "K")
+                    weight = 10;
+                if (value == "A")
+                    weight = 11;
+                this.deck.push(new Card(suit, value, weight));
             }
         }
         return this.deck.length;
@@ -36,39 +42,28 @@ class Deck {
     }
     
     deal() {
-        while(hand.length < 2) {
-            hand.push(this.deck.pop());
+        while (hand.length < 2) {
+            hand.push(this.deck.shift());
         }
         return hand;
-
     }
 
     hit() {
-        hand.push(this.deck.pop());
+        hand.push(this.deck.shift());
         // console.log(hand);
         return hand;
     }
 }
 
-
 let deck = new Deck();
-let deck = new Deck();
-
-// Perhaps link this to a 
-function addDeck(){
-    
-}
-
 // deck.createDeck(suits, values);
+// deck.shuffle();
+// console.log(deck.createDeck(suits, values));
 
-console.log(deck.createDeck(suits, values));
-// // deck.createDeck(suits, values);
-
-// // console.log(deck);
-// // console.log(deck.hit);
-console.log(deck.createDeck(suits, values));
-console.log(deck.shuffle());
-console.log(deck.deal());
-console.log(deck.hit());
+// console.log(deck.createDeck(suits, values));
+// console.log(deck.shuffle());
+// console.log(deck.deal());
+// console.log(deck.deal());
+// console.log(deck.hit());
 
 module.exports = Deck;
--- logic/game.js
+++ logic/game.js
@@ -1,19 +1,65 @@
 const Deck = require("./deck");
-const Player = require("./player");
+// const Player = require("./player");
+var suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let hand = [];
+let deck = new Deck;
+
 
 function startGame() {
-    let deck = new Deck();
     deck.createDeck(suits, values);
+    // console.log(deck.createDeck(suits, values))
+    shuffleDeck();
+    // console.log(deck);
+    createPlayers();
+    dealHands();
+}
+
+function shuffleDeck() {
     for (let i = 0; i < 7; i++) {
         deck.shuffle();
     }
 }
 
-document.getElementById("hit-btn").addEventListener("click", function() {
-    
-});
-document.getElementById("stay-btn")
+function createPlayers(num) {
+    players = new Array();
+    num = 2;
+    for(let i = 1; i <= num; i++) {
 
-document.getElementById("play-btn").addEventListener("click", function () {
-    startGame();
-});
+        var player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand };
+        players.push(player);
+    }
+    console.log(players)
+    // dealHands();
+}
+
+function dealHands() {
+    console.log(deck);
+    // for(let i = 0; i < 2; i++) {
+        for (let x = 0; x < players.length; x++) {
+            players[x].Hand.push(deck.deal());
+            console.log(players[x].Hand);
+            // deck.shuffle();
+        }
+    // }
+}
+
+function renderCard(card, player) {
+
+}
+
+// AWAITING FOR LAUREN'S HTML
+// function createPlayerUI() {
+
+// }
+
+startGame();
+
+// document.getElementById("hit-btn").addEventListener("click", function() {
+    
+// });
+// document.getElementById("stay-btn")
+
+// document.getElementById("play-btn").addEventListener("click", function () {
+//     startGame();
+// });
