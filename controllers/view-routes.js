d7cd42fd0dec65bc889585eca48d224b01404626
Connor <melty100@gmail.com>
2020-10-10 16:26:44 -0700

    Added sequelize to index route and handlebars to index file for top5 display

--- controllers/view-routes.js
+++ controllers/view-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
+var db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -21,7 +22,14 @@ module.exports = function (app) {
       // Here we've add our isAuthenticated middleware to this route.
       // If a user who is not logged in tries to access this route they will be redirected to the signup page
       app.get("/index", isAuthenticated, function(req, res) {
-        res.render("index", {});
+        db.User.findAll({
+          limit: 5
+        })
+        .then((user) => {
+          console.log(user);
+          res.render("index", {user: user});
+        })
+        .catch((err) => console.log(err));
         //res.sendFile(path.join(__dirname, "../public/index.html"));
       });
 
