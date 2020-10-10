bc081623e8231c22254b32e1f59973bd1e980842
Lauren Hooper <lluther921@gmail.com>
2020-10-10 13:44:44 -0700

    commented out helper function until errors can be resolved

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ app.use(express.json());
 // usage: {{#limit items limit="10"}} : items 0 thru 9
 // usage: {{#limit items offset="3"}} : items 3 thru context.length
 // defaults are offset=0, limit=5
-Handlebars.registerHelper('limit', function(context, block) {
-  var ret = "",
-      offset = parseInt(block.hash.offset) || 0,
-      limit = parseInt(block.hash.limit) || 5,
-      i = (offset < context.length) ? offset : 0,
-      j = ((limit + offset) < context.length) ? (limit + offset) : context.length;
+// Handlebars.registerHelper('limit', function(context, block) {
+//   var ret = "",
+//       offset = parseInt(block.hash.offset) || 0,
+//       limit = parseInt(block.hash.limit) || 5,
+//       i = (offset < context.length) ? offset : 0,
+//       j = ((limit + offset) < context.length) ? (limit + offset) : context.length;
 
-  for(i,j; i<j; i++) {
-    ret += block(context[i]);
-  }
+//   for(i,j; i<j; i++) {
+//     ret += block(context[i]);
+//   }
 
-  return ret;
-});
+//   return ret;
+// });
 
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
